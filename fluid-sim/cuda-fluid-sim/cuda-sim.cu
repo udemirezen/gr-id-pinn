9e73c470fe2726534d9cb8ba74b44576dd094aca
Phil Travis <physicistphil@gmail.com>
2020-10-08 14:58:04 -0700

    Skeleton (spooktober!) of CUDA-based fluid sim

---
+++ fluid-sim/cuda-fluid-sim/cuda-sim.cu
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <cuda_runtime_api.h>
+#include <cuda.h>
+
+#include "cuda-simd.hpp"
+
+int main(int argc, char argv[]) {    
+
+
+    return 0;
+}
---
+++ fluid-sim/cuda-fluid-sim/cuda-sim.hpp
@@ -0,0 +1,26 @@
+#ifndef CUDA_SIM_HPP
+#define CUDA_SIM_HPP
+
+void bilinear_interpolate(){
+
+}
+
+void advect() {
+
+}
+
+void diffuse() {
+
+}
+
+void addForces() {
+
+}
+
+void computePressure() {
+
+}
+
+void subtractForces() {
+
+}
