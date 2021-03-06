72221041d520128d455fcaa75adacbc8a53dbfc1
erik wessel <ekwessel@mac.com>
2021-03-06 04:21:21 -0700

    Made the variable names slightly less bad, added training samples, loss functions, and training.' does not appear to be a git repository

--- burgers.py
+++ burgers.py
@@ -9,6 +9,7 @@ import numpy as np
 import matplotlib as mpl
 mpl.use('tkagg')
 import matplotlib.pyplot as plt
+from pyDOE import lhs
 
 seed = 84
 np.random.seed(seed)
@@ -81,55 +82,100 @@ def f(t, x):
 
 # Create place-holders for input & temporary variables
 with tf.name_scope("Placeholders"):
-    f_t = tf.compat.v1.placeholder(tf.float32, shape=[None])
-    f_x = tf.compat.v1.placeholder(tf.float32, shape=[None])
-    u_t_init = tf.compat.v1.placeholder(tf.float32, shape=[None])
-    u_x_init = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    t_int = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    x_int = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    t_init = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    x_init = tf.compat.v1.placeholder(tf.float32, shape=[None])
     u_init = tf.compat.v1.placeholder(tf.float32, shape=[None])
-    u_t_boundary = tf.compat.v1.placeholder(tf.float32, shape=[None])
-    u_x_boundary = tf.compat.v1.placeholder(tf.float32, shape=[None])
-    u_boundary = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    t_boundary_xmin = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    x_boundary_xmin = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    t_boundary_xmax = tf.compat.v1.placeholder(tf.float32, shape=[None])
+    x_boundary_xmax = tf.compat.v1.placeholder(tf.float32, shape=[None])
+
 
 # Create tf graphs for calculating stuff
-u_init_pred = u(u_t_init, u_x_init)
-u_boundary_pred = u(u_t_boundary, u_x_boundary)
-u_t_boundary_pred = u_t(u_t_boundary, u_x_boundary)
-u_x_boundary_pred = u_x(u_t_boundary, u_x_boundary)
-f_pred = f(f_t, f_x)
-
-# Define loss function and optimization algorithm.
-#To-do
+u_init_pred = u(t_init, x_init)
+u_boundary_xmin_pred = u(t_boundary_xmin, x_boundary_xmin)
+u_boundary_xmax_pred = u(t_boundary_xmax, x_boundary_xmax)
+f_pred = f(t_int, x_int)
 
 # Initialize variables, create session:
 sess = tf.compat.v1.Session()
 init = tf.compat.v1.global_variables_initializer()
 sess.run(init)
 
-# Define boundary and initial conditions
-u_left = 0.
-u_right = 0.
+# Define initial conditions
 def u_0(x):
-    return np.sin(np.pi*((2.*(x-x_min)/(x_max-x_min))-1.))
+    return -np.sin(np.pi*((2.*(x-x_min)/(x_max-x_min))-1.))
+
+# Define loss function and optimization algorithm.
+#To-do
+
+initial_loss = tf.reduce_mean(tf.square(u_init_pred - u_init))
+interior_loss = tf.reduce_mean(tf.square(f_pred))
+boundary_loss = tf.reduce_mean(tf.square(u_boundary_xmin_pred - u_boundary_xmax_pred))
+
+loss = initial_loss + interior_loss + boundary_loss
+
+optimizer = tf.contrib.opt.ScipyOptimizerInterface(loss,
+                                                   method='L-BFGS-B',
+                                                   options={'maxiter':40000,
+                                                            'maxfun':100000,
+                                                            'maxcor': 50,
+                                                            'maxls': 50,
+                                                            'ftol': 1.0*np.finfo(float).eps})
+
 
 # Generate training / regularization data
 #To-do
-
-# Train
-#To-do
-
-# Display
-
-# Do a simple forward pass
 N_t = 100
 N_x = 100
 N_consistency = 1000
 t = np.linspace(t_min, t_max, N_t)
 x = np.linspace(x_min, x_max, N_x)
 
-u_init_pred_result = sess.run(u_init_pred, {u_x_init:x, u_t_init:np.zeros(N_x)})
+#T, X = np.meshgrid(t, x)
+## What is X_star???
+## Has shape (N_pred ** 3, 3)
+#X_star = np.hstack((T.flatten()[:, None], X.flatten()[:, None]))
+## Domain bounds for t, x, and v. Equivelant to taking +/- of largest {t,x,v}_max value (I think)
+#lb = X_star.min(0)
+#ub = X_star.max(0)
+
+lb = np.array([t_min, x_min])
+ub = np.array([t_max, x_max])
+
+# Train
+
+# stack of t, x for each sample
+X_initial_train = np.stack((np.zeros(N_x), # t for each sample
+                            np.linspace(x_min, x_max, N_x))) # x for each sample
+Y_initial_train = u_0(X_initial_train[1,:])
+
+X_interior_train = lb + (ub - lb)*lhs(2, N_consistency)
+
+tf_dict = {t_init: X_initial_train[0,:],
+           x_init: X_initial_train[1,:],
+           u_init: Y_initial_train,
+           t_int: X_interior_train[:, 0],
+           x_int: X_interior_train[:, 1],
+           t_boundary_xmin: t,
+           x_boundary_xmin: x_min*np.ones(N_t),
+           t_boundary_xmax: t,
+           x_boundary_xmax: x_max*np.ones(N_t)}
+
+optimizer.minimize(sess, feed_dict=tf_dict,
+                   fetches=[loss, interior_loss, initial_loss, boundary_loss])
+
+# Display
+
+# Do a simple forward pass
+u_init_pred_result = sess.run(u_init_pred, {x_init:x, t_init:np.zeros(N_x)})
+u_pred_result = sess.run(u_init_pred, {x_init:x, t_init:0.3*np.ones(N_x)})
 
 plt.plot(x, u_init_pred_result, label="NN")
 plt.plot(x, u_0(x), label="Initial Condition")
+plt.plot(x, u_pred_result, label="NN at t=0.3")
 plt.legend()
 plt.show()
 
